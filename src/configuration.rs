3f10597a10051791add4606b903e3f164bf54f23
J. Jaime Paredes <jjpp109309@gmail.com>
2024-04-21 13:52:10 -0600

    Add cofiguration file and init server from its values

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,7 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub applicatin_port: u16,
+    pub application_port: u16,
 }
 
 #[derive(serde::Deserialize)]
@@ -11,4 +11,12 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new("configuration.yaml", config::FileFormat::Yaml))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
 }
