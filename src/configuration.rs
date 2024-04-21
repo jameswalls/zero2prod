a05768e26bbd8243b2ccdc31ae5a8add1358c29a
J. Jaime Paredes <jjpp109309@gmail.com>
2024-04-20 23:32:22 -0600

    add configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,14 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub applicatin_port: u16
+}
 
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
