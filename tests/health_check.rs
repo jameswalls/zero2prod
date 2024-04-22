68adfc37f78b1b4029590beacdc6441f27b95ad3
J. Jaime Paredes <jjpp109309@gmail.com>
2024-04-21 18:25:26 -0600

    Randomize dbs to isolate integration tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,55 @@
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
+
+    let server = run(listener, connection_pool.clone())
+        .expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    TestApp { address, db_pool: connection_pool }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // create database
+    let mut connection = PgConnection::connect(
+            &config.connection_string_without_db()
+        )
+        .await
+        .expect("Failed to connect to postgres.");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("FAiled to create database.");
+
+    // migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database.");
+    
+    connection_pool
 }
 
 #[tokio::test]
@@ -24,26 +68,6 @@ async fn health_check_works() {
     // assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-async fn spawn_app() -> TestApp {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
-
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(
-            &configuration.database.connection_string()
-        )
-        .await
-        .expect("Failed to connect to Postgres.");
-
-    let server = run(listener, connection_pool.clone())
-        .expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-
-    TestApp { address, db_pool: connection_pool }
 }
 
 #[tokio::test]
