b1459daaac19095c729a4452928231fb7a732828
J. Jaime Paredes <jjpp109309@gmail.com>
2024-05-21 23:56:24 -0600

    Add log tracer for actix logs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
+use tracing_log::LogTracer;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
@@ -8,10 +9,10 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
     let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
