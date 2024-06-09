a96dc0829e2b10bb539415cd8394f141107099db
J. Jaime Paredes <jjpp109309@gmail.com>
2024-06-09 13:06:44 -0600

    formatter

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -21,18 +21,12 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish()
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-#[tracing::instrument(
-    name = "Saving new subscriber details on database",
-    skip(form, pool)
-)]
-pub async fn insert_subscriber(
-    pool: &PgPool,
-    form: &FormData,
-) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Saving new subscriber details on database", skip(form, pool))]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
