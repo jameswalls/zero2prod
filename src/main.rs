80f00fd0d20829b5464af244258e20c08c4a1e85
J. Jaime Paredes <jjpp109309@gmail.com>
2024-06-09 11:33:22 -0600

    Formatter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
