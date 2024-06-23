6ec69ae7d5b9e63be8326362f2e24a496897ac31
J. Jaime Paredes <jjpp109309@gmail.com>
2024-06-22 18:24:37 -0600

    fix non async method

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool =
         PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .await
             .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
